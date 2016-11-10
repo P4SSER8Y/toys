ad7e14508431ecb97cfdfa3cf97109ca8f58859f
KNCheung <beobachter70@163.com>
2016-11-10 19:54:33 +0800

    add script of fxxk PRIME

---
+++ fxxkPRIME/fxxkPRIME.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace fxxkPRIME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] py = System.Diagnostics.Process.GetProcessesByName("pythonw");
+            foreach (var VARIABLE in py)
+            {
+                if (VARIABLE.MainModule.FileName.Contains("PIME"))
+                {
+                    System.Console.WriteLine("{0}: {1}", VARIABLE.Id, VARIABLE.MainModule.FileName);
+                    VARIABLE.Kill();
+                }
+            }
+        }
+    }
+}
